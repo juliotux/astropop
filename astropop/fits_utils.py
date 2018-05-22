ee6ae720ce2f33b257dbd2ad6060d93724ad0b88
Julio Campagnolo <juliocampagnolo@gmail.com>
2018-05-22 20:01:35 -0300

    added a yeild generator for iterate over fits files

--- astropop/fits_utils.py
+++ astropop/fits_utils.py
@@ -1,8 +1,10 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
+import os
 import six
 import numpy as np
 from astropy.io import fits
+import functools
 
 __all__ = ['imhdus', 'check_header_keys', 'check_hdu']
 
@@ -50,3 +52,47 @@ def check_hdu(data):
         else:
             raise ValueError('The given data is not a valid CCDData type.')
     return data
+
+
+def fits_yielder(return_type, file_list, ext=0, append_to_name=None,
+                 save_to=None, overwrite=True):
+    """Create a generator object of the file_list."""
+    if save_to:
+        if not os.path.exists(save_to):
+            os.makedirs(save_to)
+        elif not os.path.isdir(save_to):
+            raise ValueError('Saving location is not a valid directory!')
+
+    if return_type == 'header':
+        func = functools.partial(fits.getheader, ext=ext)
+    elif return_type == 'data':
+        func = functools.partial(fits.getdata, ext=ext)
+    elif return_type == 'hdu':
+        func = functools.partial(check_hdu, default_hdu=ext)
+    else:
+        raise ValueError('Generator not recognized.')
+
+    def _save(old, new, yielded):
+        hdul = fits.open(old)
+        index = hdul.index_of(ext)
+        if return_type == 'header':
+            hdul[index].header = yielded
+        elif return_type == 'data':
+            hdul[index].data = yielded
+        elif return_type == 'hdu':
+            hdul[index] = yielded
+
+        hdul.writeto(save_fname, overwrite=True)
+
+    for i in file_list:
+        obj = func(i)
+        yield obj
+
+        if save_to:
+            basename = os.path.basename(i)
+            if append_to_name:
+                base, ext = os.path.splitext(basename)
+                basename = base + append_to_name + ext
+
+            save_fname = os.path.join(save_to, basename)
+            _save(i, save_fname, obj)
