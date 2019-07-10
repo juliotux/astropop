eb1a94cf292d4729d8e0c0b5517be4099740bef5
Julio Campagnolo <juliocampagnolo@gmail.com>
2019-07-10 20:08:06 -0300

    pipelines: more implementing, not working yet

--- astropop/pipelines/_base.py
+++ astropop/pipelines/_base.py
@@ -2,19 +2,16 @@
 
 import abc
 import yaml
-from collections import OrderedDict
+from functools import partial
 
-from ..logger import logger
+from ..logger import logger, log_to_list
+from ..py_utils import IndexedDict
 
 
 # TODO: __str__, __repr__, print functions for all classes
 
 
-__all__ = ['Config']
-
-
-class IncompatibilityError(RuntimeError):
-    """Error to be raised for come incompatibility."""
+__all__ = ['Config', 'Product', 'Instrument', 'Manager', 'Factory']
 
 
 def info_dumper(infos):
@@ -28,7 +25,7 @@ def info_dumper(infos):
 class _GenericConfigClass(abc.ABC):
     """Class for generic sotring configs. Like a powered dict."""
     _frozen = False
-    _prop_dict = OrderedDict()
+    _prop_dict = IndexedDict()
     _mutable_vars = ['_frozen', 'logger']
     logger = logger
 
@@ -147,42 +144,46 @@ class Instrument(_GenericConfigClass):
 
 class Product():
     """Store all informations and data of a product."""
-    _product_manager = None  # Parent product manager
-    _capabilities = []  # The product capabilities
     _destruct_callbacks = []  # List of destruction callbacks
     _log_list = []  # Store the logs
-    _infos = OrderedDict()  # Custom product informations
-    _mutable_vars = ['_product_manager']  # Variables that can be changed
+    _variables = {}
+    _infos = IndexedDict()
+    _mutable_vars = ['_logger']  # Variables that can be assigned
     _instrument = None  # Product instrument
+    _logger = None
 
-    # Product do not subclass _GenericConfigClass to keep variables
-    # acessing more customized.
-    def __init__(self, product_manager=None, instrument=None, index=None,
+    def __init__(self, manager=None, instrument=None, index=None,
                  **kwargs):
-        if product_manager is None:
+        if manager is None:
             raise ValueError("A product has to be created with a"
-                             " product_manager.")
+                             " manager.")
         # if not isinstance(product_manager, ProductManager):
         #     raise ValueError("product_manager is not a valid ProductManager "
         #                      "instance.")
-        self._product_manager = product_manager
+        self._manager = manager
         self._instrument = instrument
-        self.index = index
-
-        # Setup the logger conveniently
-        self._logger = self._product_manager.logger.getChild('product')
-        log_to_list(self._logger, self._log_list)
 
         for name, value in kwargs.items():
             self.__setattr__(name, value)
 
     @property
-    def logger(self):
-        return self.logger
+    def index(self):
+        return self._manager.get_product_index(self.name)
 
     @property
-    def product_manager(self):
-        return self._product_manager
+    def name(self):
+        return self._manager.get_product_name(self)
+
+    @property
+    def logger(self):
+        if self._logger is None:
+            self._logger = self._manager.logger.getChild(self.name)
+            log_to_list(self._logger, self._log_list)
+        return self._logger
+
+    @property
+    def manager(self):
+        return self._manager
 
     @property
     def capabilities(self):
@@ -197,15 +198,15 @@ class Product():
         """Print general informations about the product, in a yaml format."""
         info_dict = self._infos.copy()
 
-        info_dict['capabilities'] = self._capabilities
+        info_dict['variables'] = self._variables
 
-        inst = OrderedDict()
+        inst = dict()
         inst['class'] = self._instrument.__class__.__name__
         inst['id'] = self._instrument._identifier
         inst['properties'] = self._instrument.properties
         info_dict['instrument'] = inst
 
-        dest = OrderedDict()
+        dest = dict()
         dest['number'] = len(self._destruct_callbacks)
         dest['functions'] = [i.__name__ for i in self._destruct_callbacks]
         info_dict['destruction_callbacks'] = dest
@@ -219,25 +220,25 @@ class Product():
         """Print the log of the product."""
         return "\n".join(self._log_list)
 
+    def get_value(self, name):
+        """Get a variable value."""
+        return self._variables[name]
+
+    def set_value(self, name, value):
+        """Set a value to a variable."""
+        self._variables[name] = value
+
     def add_info(self, session, info_dict):
         """Custom add information dictionaries to the product."""
         if session in self._infos.keys():
             self.logger.warn('Session {} already exists in this product infos.'
                              ' Overwriting it.'
                              .format(session))
-        if session in ['history', 'capabilities', 'destruction_callbacks']:
+        if session in ['variables', 'history', 'destruction_callbacks']:
             self.logger.warn('{} is a protected name of session. Skipping.'
                              .format(session))
             return
         self._infos[session] = info_dict
-
-    def add_capability(self, capability):
-        if capability not in self._capabilities:
-            self._capabilities.append(capability)
-
-    def del_capability(self, capability):
-        if capability in self._capabilities:
-            self._capabilities.remove(capability)
 
     def add_destruct_callback(self, callback, *args, **kwargs):
         """Add a destruction callback. First argument must be a class slot,
@@ -254,44 +255,6 @@ class Product():
                 logger.debug("Destruction callback {} problem. Error: {}"
                              .format(i, e))
 
-    def update(self, config):
-        for k, v in config.items():
-            self.__setitem__(k, v)
-
-    def items(self):
-        return self.__dict__.items()
-
-    def keys(self):
-        return self.__dict__.keys()
-
-    def __getitem__(self, name):
-        return self.__dict__.__getitem__(name)
-
-    def __getattr__(self, name):
-        if name in self.__dict__.keys():
-            return self.__getitem__(name)
-        else:
-            super().__getattribute__(name)
-
-    def __setitem__(self, name, value):
-        self.__dict__.__setattr__(name, value)
-
-    def __setattr__(self, name, value):
-        if name not in self.__class__.__dict__.keys():
-            self.__setitem__(name, value)
-        elif name not in self._mutable_vars:
-            super().__setattr__(name, value)
-        else:
-            raise KeyError('{} is a protected variable.'.format(name))
-
-    def __delattr__(self, name):
-        if name in self.__dict__.keys():
-            del self.__dict__[name]
-        else:
-            super().__delattr__(name)
-
-    def __str__(self):
-        return "Product: {}\n{}".format(self.__class__.__name__, self.info)
 
 class Stage(abc.ABC):
     """Stage process (sub-part) of a pipeline."""
@@ -325,20 +288,105 @@ class Stage(abc.ABC):
         self.run(product, config)
 
 
-class Manager():
-    _stages = OrderedDict()  # Stages list
-    _register = OrderedDict()  # Variables registering
+class Factory():
+    _stages = IndexedDict()  # Stages list
+    _register = IndexedDict()  # Variables registering
     _config = Config()
     _targets = []  # Stage targets for pipeline execution
     _active_product = None
 
+    """Class to handle execution and product/stage interface."""
 
-class Boss(abc.ABC):
+
+class Manager(abc.ABC):
     _config = Config()
-    _products = OrderedDict()
+    _products = IndexedDict()
+    _factory = Factory()
 
     """Class to handle the general pipeline management."""
     def __init__(self, config_file=None):
         if config_file is not None:
             with open(config_file, 'r') as stream:
                 self._config.update(yaml.load(stream))
+
+    @abs.abstractmethod
+    def setup_products(self, *args, **kwargs):
+        """Setup products based on specified parameters.
+
+        Examples:
+            - Read files from a folder, with filters
+            - List files
+            - Load a configure file
+        """
+
+    def add_product(self, name, product, index=None, requires=[]):
+        """Add a new product to this Manager.
+
+        Parameters:
+            name : string
+                Indentifying name for this product. Uniqueness checked.
+            product : `Product`
+                Product valid instance.
+            index : int
+                Insert the product in a given index.
+            requires : list(string)
+                List of products to be processed first.
+        """
+        index1 = None
+        try:
+            inds = [self._products.index(r)
+                    for r in requires]
+            if len(inds) > 0:
+                index1 = max(inds)
+
+        if index1 is not None and index is not None:
+            if index1 < index:
+                logger.warn('Cannot insert product {} before its requirements.'
+                            ' Overwriting to {}'.format(index, index1))
+                index = index1
+
+        if index is None:
+            # If indexes are not set, just append
+            self._products[name] = product
+        else index is not None:
+            self._products.insert_at(index, name, product)
+
+    def get_product_index(self, name):
+        """Return the current index of a given product."""
+        for i, k in enumerate(self._products.keys()):
+            if k == name:
+                return i
+
+        self.logger.warn("Product {} not found.".format(name))
+
+    def get_product_name(self, instance):
+        """Return the name of a product based on its instance."""
+        for i, v in self._products.items():
+            if v == instance:
+                return i
+
+        self.logger.warn("No product associated to {} instance."
+                         .format(str(instance)))
+
+    def del_product(self, name):
+        """Remove and clean a product."""
+        try:
+            del self._products[name]
+        except KeyError:
+            logger.debug("Product {} not in this factory.".format(name))
+            pass
+
+    def register_stage(self, name, stage, disable_variables):
+        """Register a stage"""
+
+    def unregister_stage(sell, name):
+        """Remove a stage from the registers."""
+
+    def get_value(self, variable):
+        """Get a value from a registered variable."""
+        # If variable not set, run the responsible stage.
+        # Check if the variable is running.
+
+    def set_variable(self, stage, variable, value):
+        """Set a variable value to product."""
+        # Check if this stage can set the variable to product.
