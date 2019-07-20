f78daa702f1dad5061469a6dcd107514fb952bf9
Julio Campagnolo <juliocampagnolo@gmail.com>
2019-07-20 12:25:39 -0300

    trim_array is necessary

--- astropop/math/array.py
+++ astropop/math/array.py
@@ -6,13 +6,33 @@
 import numpy as np
 
 
-__all__ = ['xy2r', 'iraf_indices']
+__all__ = ['xy2r', 'iraf_indices', 'trim_array']
 
 
 def xy2r(x, y, data, xc, yc):
     """Convert (x, y) values to distance of a (xc, yc) position."""
     r = np.hypot((x-xc), (y-yc))
     return np.ravel(r), np.ravel(data)
+
+
+def trim_array(data, box_size, position, indices=None):
+    """Trim a numpy array around a position."""
+    x, y = position
+    dx = dy = float(box_size)/2
+
+    x_min = max(int(x-dx), 0)
+    x_max = min(int(x+dx)+1, data.shape[1])
+    y_min = max(int(y-dy), 0)
+    y_max = min(int(y+dy)+1, data.shape[0])
+
+    d = data[y_min:y_max, x_min:x_max]
+
+    if indices is None:
+        return d, x-x_min, y-y_min
+    else:
+        xi = indices[1][y_min:y_max, x_min:x_max]
+        yi = indices[0][y_min:y_max, x_min:x_max]
+        return d, xi, yi
 
 
 def iraf_indices(data):
