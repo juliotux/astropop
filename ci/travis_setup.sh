9fc9cb25ff67b08cce8542c46d8a9b2cb03d8456
Julio Campagnolo <juliocampagnolo@gmail.com>
2019-12-26 17:27:38 -0300

    retry travis

--- ci/travis_setup.sh
+++ ci/travis_setup.sh
@@ -12,6 +12,7 @@ sudo apt-get update
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 bash miniconda.sh -b -p "$HOME/miniconda"
 source "$HOME/miniconda/etc/profile.d/conda.sh"
+conda init bash
 hash -r
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
