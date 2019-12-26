bcda17db803f7d602ac649d946512fd28b4551e0
Julio Campagnolo <juliocampagnolo@gmail.com>
2019-12-26 20:03:14 -0300

    another small fix

--- ci/travis_setup.sh
+++ ci/travis_setup.sh
@@ -13,9 +13,9 @@ echo "-----------------------------------------------"
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 bash miniconda.sh -b -p "$HOME/miniconda"
 source "$HOME/miniconda/etc/profile.d/conda.sh"
+conda config --set always_yes yes --set changeps1 no
 conda update conda
 hash -r
-conda config --set always_yes yes --set changeps1 no
 
 # Create test environment
 echo "-----------------------------------------------"
