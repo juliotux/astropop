99cc132e2ba7f6d727c8cfff6070c3acacca846d
Julio Campagnolo <juliocampagnolo@gmail.com>
2019-12-28 11:43:56 -0300

    Try other python versions with no channel priority

--- ci/travis_setup.sh
+++ ci/travis_setup.sh
@@ -17,6 +17,7 @@ wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O mi
 bash miniconda.sh -b -p "$MINICONDA"
 source "$MINICONDA/etc/profile.d/conda.sh"
 conda config --set always_yes yes --set changeps1 no
+conda config --set channel_priority false
 conda update conda
 hash -r
 
