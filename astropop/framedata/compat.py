2c697f3caeb59a3e6f83157c7814e2bd12144834
Julio Campagnolo <juliocampagnolo@gmail.com>
2020-04-16 03:37:51 -0300

    self implementation of wcs header cleaning

--- astropop/framedata/compat.py
+++ astropop/framedata/compat.py
@@ -1,9 +1,16 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""Handle compatibility between FrameData and other data formats."""
 
+import itertools
 from astropy.io import fits
+from astropy.wcs import WCS
 
 
-__all__ = ['_unsupport_fits_open_keywords', 'imhdus', 'EmptyDataError']
+from ..logger import logger
+
+
+__all__ = ['_unsupport_fits_open_keywords', 'imhdus', 'EmptyDataError',
+           'extract_header_wcs']
 
 
 _unsupport_fits_open_keywords = {
@@ -14,6 +21,66 @@ _unsupport_fits_open_keywords = {
 
 imhdus = (fits.ImageHDU, fits.PrimaryHDU, fits.CompImageHDU,
           fits.StreamingHDU)
+
+
+_PCs = set(['PC1_1', 'PC1_2', 'PC2_1', 'PC2_2'])
+_CDs = set(['CD1_1', 'CD1_2', 'CD2_1', 'CD2_2'])
+_KEEP = set(['JD-OBS', 'MJD-OBS', 'DATE-OBS'])
+
+
+def extract_header_wcs(header, logger=logger):
+    """Get a header (or dict) and extract a WCS based on the keys.
+
+    Parameters
+    ----------
+    header : `~astropy.fits.Header` or dict_like
+        Header to extract the WCS.
+    logger : `~logging.Logger` (optional)
+        Logger instance to log warnings.
+
+    Returns
+    -------
+    header : `~astropy.fits.Header`
+        Header cleaned from WCS keys.
+    wcs : `~astropy.wcs.WCS` os `None`
+        World Coordinate Sistem extracted from the header.    
+    """
+    header = header.copy()  # Ensure original header will not be modified
+
+    # First, check if there is a WCS. If not, return header and None WCS
+    try:
+        wcs = WCS(header, relax=True)
+        if not wcs.wcs.ctype[0]:
+            wcs = None
+    except Exception as e:
+        logger.warning('An error raised when extracting WCS: %s', e)
+        wcs = None
+
+    if wcs is not None:
+        # Delete wcs keys, except time ones
+        for k in wcs.to_header(relax=True).keys():
+            if k not in _KEEP:
+                header.remove(k, ignore_missing=True)
+
+        # Astropy uses PC. So remove CD if any
+        # The reverse case should not be allowed by astropy
+        if (_PCs & set(wcs.to_header(relax=True))) and (_CDs & set(header)):
+            for k in _CDs:
+                header.remove(k, ignore_missing=True)
+
+        # Check and remove remaining SIP coefficients
+        if wcs.sip is not None:
+            kwd = '{}_{}_{}'
+            pol = ['A', 'B', 'AP', 'BP']
+            for poly in pol:
+                order = wcs.sip.__getattribute__(f'{poly.lower()}_order')
+                for i, j in itertools.product(range(order), repeat=2):
+                    header.remove(kwd.format(poly, i, j),
+                                  ignore_missing=True)
+                header.remove(f'{poly}_ORDER', ignore_missing=True)
+                header.remove(f'{poly}_DMAX', ignore_missing=True)
+
+    return (header, wcs)
 
 
 class EmptyDataError(ValueError):
---
+++ astropop/framedata/tests/test_compat.py
@@ -0,0 +1,158 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+# Some parts stolen from Astropy CCDData testing bench
+
+from astropop.framedata.compat import extract_header_wcs
+from astropy.io import fits
+from astropy.wcs import WCS
+import pytest_check as check
+
+
+_comon_wcs_keys = ('CTYPE', 'CRVAL', 'CRPIX', 'CD1_', 'CD2_', 'PC1_', 'PC2_')
+
+
+_base_header = """SIMPLE  =                    T / Fits standard
+BITPIX  =                  -32 / FOUR-BYTE SINGLE PRECISION FLOATING POINT
+NAXIS   =                    2 / STANDARD FITS FORMAT
+NAXIS1  =                  256 / STANDARD FITS FORMAT
+NAXIS2  =                  256 / STANDARD FITS FORMAT
+DATE-OBS= '1996-10-14T10:14:36.123' / Date and time of start of obs. in UTC.
+"""
+
+_wcs_no_sip = """
+CTYPE1  = 'RA---TAN'       / Tangent projection
+CTYPE2  = 'DEC--TAN'       / Tangent projection
+CRVAL1  =     202.482322805429 / [deg] RA at CRPIX1,CRPIX2
+CRVAL2  =     47.1751189300101 / [deg] DEC at CRPIX1,CRPIX2
+CRPIX1  =                 128. / Reference pixel along axis 1
+CRPIX2  =                 128. / Reference pixel along axis 2
+CD1_1   = 0.000249756880272355 / Corrected CD matrix
+CD1_2   = 0.000230177809743655 / Corrected CD matrix
+CD2_1   = 0.000230428519265417 / Corrected CD matrix element
+CD2_2   = -0.000249965770576587 / Corrected CD matrix element
+"""
+
+_wcs_sip = """
+CTYPE1  = 'RA---TAN-SIP'       / RA---TAN with distortion in pixel
+CTYPE2  = 'DEC--TAN-SIP'       / DEC--TAN with distortion in pixel
+CRVAL1  =     202.482322805429 / [deg] RA at CRPIX1,CRPIX2
+CRVAL2  =     47.1751189300101 / [deg] DEC at CRPIX1,CRPIX2
+CRPIX1  =                 128. / Reference pixel along axis 1
+CRPIX2  =                 128. / Reference pixel along axis 2
+CD1_1   = 0.000249756880272355 / Corrected CD matrix element
+CD1_2   = 0.000230177809743655 / Corrected CD matrix element
+CD2_1   = 0.000230428519265417 / Corrected CD matrix element
+CD2_2   = -0.000249965770576587 / Corrected CD matrix element
+A_ORDER =                    3 / polynomial order, axis 1, detector to sky
+A_0_2   =           2.9656E-06 / distortion coefficient
+A_0_3   =           3.7746E-09 / distortion coefficient
+A_1_1   =           2.1886E-05 / distortion coefficient
+A_1_2   =          -1.6847E-07 / distortion coefficient
+A_2_0   =          -2.3863E-05 / distortion coefficient
+A_2_1   =           -8.561E-09 / distortion coefficient
+A_3_0   =          -1.4172E-07 / distortion coefficient
+A_DMAX  =                1.394 / [pixel] maximum correction
+B_ORDER =                    3 / polynomial order, axis 2, detector to sky
+B_0_2   =             2.31E-05 / distortion coefficient
+B_0_3   =          -1.6168E-07 / distortion coefficient
+B_1_1   =          -2.4386E-05 / distortion coefficient
+B_1_2   =          -5.7813E-09 / distortion coefficient
+B_2_0   =           2.1197E-06 / distortion coefficient
+B_2_1   =          -1.6583E-07 / distortion coefficient
+B_3_0   =          -2.0249E-08 / distortion coefficient
+B_DMAX  =                1.501 / [pixel] maximum correction
+AP_ORDER=                    3 / polynomial order, axis 1, sky to detector
+AP_0_1  =          -6.4275E-07 / distortion coefficient
+AP_0_2  =          -2.9425E-06 / distortion coefficient
+AP_0_3  =           -3.582E-09 / distortion coefficient
+AP_1_0  =          -1.4897E-05 / distortion coefficient
+AP_1_1  =           -2.225E-05 / distortion coefficient
+AP_1_2  =           1.7195E-07 / distortion coefficient
+AP_2_0  =           2.4146E-05 / distortion coefficient
+AP_2_1  =            6.709E-09 / distortion coefficient
+AP_3_0  =           1.4492E-07 / distortion coefficient
+BP_ORDER=                    3 / polynomial order, axis 2, sky to detector
+BP_0_1  =          -1.6588E-05 / distortion coefficient
+BP_0_2  =          -2.3424E-05 / distortion coefficient
+BP_0_3  =            1.651E-07 / distortion coefficient
+BP_1_0  =          -2.6783E-06 / distortion coefficient
+BP_1_1  =           2.4753E-05 / distortion coefficient
+BP_1_2  =           3.8917E-09 / distortion coefficient
+BP_2_0  =           -2.151E-06 / distortion coefficient
+BP_2_1  =              1.7E-07 / distortion coefficient
+BP_3_0  =           2.0482E-08 / distortion coefficient
+"""
+
+_invalid_wcs = """
+CRVAL1  =         164.98110962 / Physical value of the reference pixel X
+CRVAL2  =          44.34089279 / Physical value of the reference pixel Y
+CRPIX1  =                -34.0 / Reference pixel in X (pixel)
+CRPIX2  =               2041.0 / Reference pixel in Y (pixel)
+CDELT1  =           0.10380000 / X Scale projected on detector (#/pix)
+CDELT2  =           0.10380000 / Y Scale projected on detector (#/pix)
+CTYPE1  = 'RA---TAN'           / Pixel coordinate system
+CTYPE2  = 'WAVELENGTH'         / Pixel coordinate system
+CUNIT1  = 'degree  '           / Units used in both CRVAL1 and CDELT1
+CUNIT2  = 'nm      '           / Units used in both CRVAL2 and CDELT2
+CD1_1   =           0.20760000 / Pixel Coordinate translation matrix
+CD1_2   =           0.00000000 / Pixel Coordinate translation matrix
+CD2_1   =           0.00000000 / Pixel Coordinate translation matrix
+CD2_2   =           0.10380000 / Pixel Coordinate translation matrix
+C2YPE1  = 'RA---TAN'           / Pixel coordinate system
+C2YPE2  = 'DEC--TAN'           / Pixel coordinate system
+C2NIT1  = 'degree  '           / Units used in both C2VAL1 and C2ELT1
+C2NIT2  = 'degree  '           / Units used in both C2VAL2 and C2ELT2
+RADECSYS= 'FK5     '           / The equatorial coordinate system
+"""
+
+
+def test_extract_header_nowcs():
+    header = fits.Header.fromstring(_base_header, sep='\n')
+    h, wcs = extract_header_wcs(header)
+    check.is_none(wcs)
+    check.is_instance(h, fits.Header)
+    check.equal(h, header)
+    check.is_false(h is header)
+
+
+def test_extract_header_nosip():
+    header = fits.Header.fromstring(_base_header+_wcs_no_sip, sep='\n')
+    h, wcs = extract_header_wcs(header)
+    check.is_instance(wcs, WCS)
+    check.equal(wcs.wcs.ctype[0], 'RA---TAN')
+    check.equal(wcs.wcs.ctype[1], 'DEC--TAN')
+    check.is_instance(h, fits.Header)
+    for i in _comon_wcs_keys:
+        check.is_not_in(f'{i}1', h.keys())
+        check.is_not_in(f'{i}2', h.keys())
+    check.is_in('DATE-OBS', h.keys())
+    check.is_false(h is header)
+    check.not_equal(h, header)
+
+
+def test_extract_header_sip():
+    header = fits.Header.fromstring(_base_header+_wcs_sip, sep='\n')
+    h, wcs = extract_header_wcs(header)
+    check.is_instance(wcs, WCS)
+    check.equal(wcs.wcs.ctype[0], 'RA---TAN-SIP')
+    check.equal(wcs.wcs.ctype[1], 'DEC--TAN-SIP')
+    check.is_instance(h, fits.Header)
+    for i in _comon_wcs_keys:
+        check.is_not_in(f'{i}1', h.keys())
+        check.is_not_in(f'{i}2', h.keys())
+    for i in ('A_0_2', 'AP_2_0', 'BP_ORDER', 'A_DMAX'):
+        check.is_not_in(i, h.keys())
+    check.is_in('DATE-OBS', h.keys())
+    check.is_false(h is header)
+    check.not_equal(h, header)
+
+
+def test_extract_invalid_wcs_header():
+    # It should no raise, just return empty wcs
+    # No header change too
+    header = fits.Header.fromstring(_base_header+_invalid_wcs, sep='\n')
+    h, wcs = extract_header_wcs(header)
+    check.is_none(wcs)
+    check.is_instance(h, fits.Header)
+    check.equal(h, header)
+    check.is_false(h is header)
