e8e13f7da12eb9ddb9836133d45d27ba196a91e0
Julio Campagnolo <juliocampagnolo@gmail.com>
2020-09-29 21:42:30 -0300

    more photometry verbosity

--- astropop/photometry/__init__.py
+++ astropop/photometry/__init__.py
@@ -2,7 +2,8 @@
 
 from .aperture import aperture_photometry
 from .detection import (background, sepfind, daofind, calc_fwhm,
-                        recenter_sources, starfind)
+                        recenter_sources, starfind, gen_filter_kernel,
+                        sources_mask)
 from .solve_photometry import (solve_photometry_median,
                                solve_photometry_average,
                                solve_photometry_montecarlo)
@@ -15,4 +16,5 @@ solve_photometry_available_methods = ['median', 'average', 'montecarlo']
 __all__ = ['aperture_photometry', 'background', 'sepfind', 'daofind',
            'calc_fwhm', 'recenter_sources', 'starfind',
            'solve_photometry_median', 'solve_photometry_average',
-           'solve_photometry_montecarlo']
+           'solve_photometry_montecarlo', 'gen_filter_kernel',
+           'sources_mask']
