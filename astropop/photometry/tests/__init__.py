8511fa153fdf04e4342d167637911d1566cf965a
rudnerlq <66256969+rudnerlq@users.noreply.github.com>
2020-12-14 13:36:22 -0300

    test_detection updated

---
+++ astropop/photometry/tests/__init__.py
@@ -0,0 +1,4 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+# from .dualbeam import *  # noqa
+from .gen_pol import (gaussian_2d, gen_image)
--- astropop/photometry/tests/test_detection.py
+++ astropop/photometry/tests/test_detection.py
@@ -14,6 +14,30 @@ from astropop.photometry.tests import (gen_image)
 def test_gen_filter_kernel():
     pass
 
+# # @pytest.mark.parametrize('r', [2, 3, 4])
+# def test_global_background():
+    
+#     # im_e = np.random.poisson(im_e) criar uma distribuição de erro poissonico 
+#     # Deve criar  um erro poissonico sobre uma distribuição gaussiana
+
+#     scale = 1.0
+#     psi = 90
+#     p = 0.1
+#     theta = 15
+    
+#     image_test = gen_image(scale, psi, p, theta)
+    
+#     image_cols = len(image_test)
+#     image_rows = len(image_test[0])
+    
+#     box_size = image_cols
+#     filter_size = image_cols
+
+#     bkg_1_global = background(image_test, box_size, filter_size, mask=None, global_bkg=True)
+
+#     npt.assert_array_almost_equal(bkg_1_global[0], 298, decimal=0)
+#     npt.assert_array_almost_equal(bkg_1_global[1], 24.9, decimal=1)
+    
 # @pytest.mark.parametrize('r', [2, 3, 4])
 def test_background():
     
@@ -34,9 +58,10 @@ def test_background():
     filter_size = image_cols
 
     bkg_1_global = background(image_test, box_size, filter_size, mask=None, global_bkg=True)
+    bkg_1 = background(image_test, box_size, filter_size, mask=None, global_bkg=False)
 
     npt.assert_array_almost_equal(bkg_1_global[0], 298, decimal=0)
-    npt.assert_array_almost_equal(bkg_1_global[1], 24.9, decimal=1)
+    npt.assert_array_almost_equal(bkg_1_global[1], 24.9, decimal=1)    
 
 # @pytest.mark.parametrize('r', [2, 3, 4])
 def test_sepfind():
