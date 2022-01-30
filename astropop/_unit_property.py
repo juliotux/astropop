79520037d4b3938e61baf745611076d033b3150c
Julio Campagnolo <juliocampagnolo@gmail.com>
2022-01-30 20:33:56 -0300

    unit_property: avoid use of self

--- astropop/_unit_property.py
+++ astropop/_unit_property.py
@@ -6,16 +6,16 @@ from astropy import units
 
 def unit_property(cls):
     """Add a `unit` property to a class."""
-    def _unit_getter(self):
-        if self._unit is None:
+    def _unit_getter(obj):
+        if obj._unit is None:
             return units.dimensionless_unscaled
-        return self._unit
+        return obj._unit
 
-    def _unit_setter(self, value):
+    def _unit_setter(obj, value):
         if value is None or units.Unit(value) == units.dimensionless_unscaled:
-            self._unit = None
+            obj._unit = None
         else:
-            self._unit = units.Unit(value)
+            obj._unit = units.Unit(value)
 
     cls._unit = None
     cls.unit = property(_unit_getter, _unit_setter,
