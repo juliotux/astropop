acc37a843d3729dcfbfcf072b9b807ff73566da5
Julio Campagnolo <juliocampagnolo@gmail.com>
2022-04-14 00:24:24 -0300

    sql: re-added necessary else

--- astropop/_db.py
+++ astropop/_db.py
@@ -295,7 +295,8 @@ class SQLColumn:
         """Set a row in the column."""
         if isinstance(key, int):
             self._db.set_item(self._table, self._name, key, value)
-        raise KeyError(f'{key}')
+        else:
+            raise KeyError(f'{key}')
 
     def __len__(self):
         """Get the number of rows in the column."""
