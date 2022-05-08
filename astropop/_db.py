small fixes in broadcast

astropop/_db.py
@@ -670,9 +670,9 @@ class SQLDatabase:
 
         dict_row_list = _dict2row(cols=self.column_names(table), **data)
         try:
-            rows = np.broadcast(**dict_row_list)
+            rows = np.broadcast(*dict_row_list)
         except ValueError:
-            rows = broadcast(**dict_row_list)
+            rows = broadcast(*dict_row_list)
         rows = list(zip(*rows.iters))
         self._add_data_list(table, rows)
 

astropop/py_utils.py
@@ -145,9 +145,12 @@ class broadcast:
         self.index += 1
         return arr
 
+    @property
     def iters(self):
         """Return the tuple containing the iterators."""
-        return tuple(i for i in self)
+        return tuple(np.array(a) if self.is_array[i]
+                     else np.array([a]*self.length)
+                     for i, a in enumerate(self.args))
 
     def __len__(self):
         """Return the length of the broadcast."""