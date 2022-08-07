8cf74028d75a3fec6ed89a8c38af3b2d1364919c
Julio Campagnolo <juliocampagnolo@gmail.com>
2022-08-07 19:32:31 -0300

    memmap: try new approach to native byteorder

--- astropop/framedata/memmap.py
+++ astropop/framedata/memmap.py
@@ -156,11 +156,13 @@ class MemMapArray:
         else:
             if not hasattr(data, 'dtype'):
                 data = np.array(data)
-            # ensure native dtype
-            dtype = dtype or data.dtype.newbyteorder('=')
+            dtype = dtype or data.dtype
             # Default dtype
-            dtype = dtype or '=float64'
+            dtype = dtype or 'float64'
             self._contained = np.array(data, dtype=dtype)
+            # ensure native byteorder
+            if not self._contained.dtype.isnative:
+                self._contained = self._contained.byteswap().newbyteorder()
         self.set_filename(filename)
         self._file_lock = True
 
@@ -269,6 +271,11 @@ class MemMapArray:
             adata = data
             if isinstance(data, MemMapArray):
                 adata = data._contained
+            if not hasattr(adata, 'dtype'):
+                adata = np.array(adata)
+            if not adata.dtype.isnative:
+                adata = adata.byteswap().newbyteorder()
+                dtype = adata.dtype
             if self.memmap:
                 name = self.filename
                 mm = self._contained
