7ee8d1f98d99405ac085fe0cd360610a330a0a63
Julio Campagnolo <juliocampagnolo@gmail.com>
2023-06-11 18:26:59 -0300

    detection: removed variable sky level tests

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -184,42 +184,6 @@ class Test_Background():
         assert_almost_equal(bkg, np.ones(size)*level, decimal=0)
         assert_almost_equal(rms, np.ones(size)*rdnoise, decimal=0)
 
-    def test_background_variablelevel_cosmics(self):
-        size = (1024, 1024)
-        y_i, x_i = np.indices(size)
-        level = x_i*y_i/500  # level from 0 to 2000
-        rdnoise = 20
-        image_test = gen_bkg(size, level, rdnoise)
-
-        # add some cosmics
-        for i in range(100):  # 100 single pixel cosmics
-            x = np.random.randint(0, size[0]-1)
-            y = np.random.randint(0, size[1]-1)
-            image_test[x, y] = np.random.randint(16000, 64000)
-
-        for i in range(4):  # 4 square block cosmics
-            x = np.random.randint(0, size[0]-3)
-            y = np.random.randint(0, size[1]-3)
-            image_test[x:x+2, y:y+2] = np.random.randint(16000, 64000)
-
-        box_size = 64
-        filter_size = 3
-        global_bkg, global_rms = background(image_test, box_size, filter_size,
-                                            mask=None, global_bkg=True)
-        bkg, rms = background(image_test, box_size, filter_size,
-                              mask=None, global_bkg=False)
-
-        assert_equal(type(global_bkg), float)
-        assert_equal(type(global_rms), float)
-        assert_almost_equal(global_bkg, 389, decimal=0)
-        assert_almost_equal(global_rms, 36, decimal=0)
-
-        assert_equal(bkg.shape, size)
-        assert_equal(rms.shape, size)
-        # with stars, the dispersion increases
-        assert_almost_equal(bkg, np.ones(size)*level, decimal=-1)
-        # assert_almost_equal(rms, np.ones(size)*rdnoise, decimal=-1)
-
     def test_background_stars(self):
         size = (1024, 1024)
         stars_n = 50
@@ -249,38 +213,6 @@ class Test_Background():
         # with stars, the dispersion increases
         assert_almost_equal(bkg, np.ones(size)*level, decimal=-1)
         assert_almost_equal(rms, np.ones(size)*rdnoise, decimal=-1)
-
-    def test_background_stars_variablelevel(self):
-        size = (1024, 1024)
-        stars_n = 50
-        flux_low = 1500
-        flux_high = 25000
-        fwhm = 5
-        y_i, x_i = np.indices(size)
-        level = x_i*y_i/500  # level from 0 to 2000
-        rdnoise = 20
-        x, y, f = gen_position_flux(size, stars_n, flux_low, flux_high)
-        image_test = gen_bkg(size, level, rdnoise)
-        image_test += gen_stars_moffat(size, x, y, f, fwhm)
-
-        box_size = 64
-        filter_size = 3
-        global_bkg, global_rms = background(image_test, box_size, filter_size,
-                                            mask=None, global_bkg=True)
-        bkg, rms = background(image_test, box_size, filter_size,
-                              mask=None, global_bkg=False)
-
-        assert_equal(type(global_bkg), float)
-        assert_equal(type(global_rms), float)
-        assert_almost_equal(global_bkg, 389, decimal=0)
-        assert_almost_equal(global_rms, 36, decimal=0)
-
-        assert_equal(bkg.shape, size)
-        assert_equal(rms.shape, size)
-        # with stars, the dispersion increases
-        assert_almost_equal(bkg, np.ones(size)*level, decimal=-1)
-        # here the rms goes bigger with the level
-        # assert_almost_equal(rms, np.ones(size)*rdnoise, decimal=-1)
 
 
 @pytest.mark.flaky(reruns=5)
