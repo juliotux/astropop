fix keyword normalization in tests

astropop/framedata/_compat.py
@@ -16,7 +16,7 @@ from astropy import units as u
 
 
 from ..logger import logger
-from ..fits_utils import imhdus
+from ..fits_utils import imhdus, string_to_header_key
 
 
 _PCs = set(['PC1_1', 'PC1_2', 'PC2_1', 'PC2_2'])
@@ -110,7 +110,7 @@ def _normalize_and_strip_dict(meta):
     comment = []
 
     for k, v in meta.items():
-        k = fits.Card.normalize_keyword(k)
+        k = string_to_header_key(k)
         # pop history and comment
         if k == 'HISTORY':
             if np.isscalar(v):

tests/test_framedata_io.py
@@ -156,8 +156,7 @@ class TestFrameData2CCDData:
         assert_equal(ccd.data, 100*np.ones(self.shape))
         assert_equal(ccd.unit, self.unit)
         for i in self.meta.keys():
-            ni = fits.Card.normalize_keyword(i)
-            assert_equal(ccd.meta[ni], self.meta[i])
+            assert_equal(ccd.meta[i], self.meta[i])
         assert_equal(ccd.mask, np.zeros(self.shape, dtype=bool))
         assert_is_none(ccd.uncertainty)
 
@@ -169,8 +168,7 @@ class TestFrameData2CCDData:
         assert_equal(ccd.data, 100*np.ones(self.shape))
         assert_equal(ccd.unit, self.unit)
         for i in self.meta.keys():
-            ni = fits.Card.normalize_keyword(i)
-            assert_equal(ccd.meta[ni], self.meta[i])
+            assert_equal(ccd.meta[i], self.meta[i])
         assert_equal(ccd.mask, self.mask)
         assert_is_none(ccd.uncertainty, str(ccd.uncertainty))
 
@@ -182,8 +180,7 @@ class TestFrameData2CCDData:
         assert_equal(ccd.data, 100*np.ones(self.shape))
         assert_equal(ccd.unit, self.unit)
         for i in self.meta.keys():
-            ni = fits.Card.normalize_keyword(i)
-            assert_equal(ccd.meta[ni], self.meta[i])
+            assert_equal(ccd.meta[i], self.meta[i])
         assert_equal(ccd.mask, np.zeros(self.shape, dtype=bool))
         assert_equal(ccd.uncertainty.array, np.ones(self.shape))
         assert_equal(ccd.uncertainty.unit, self.unit)
@@ -198,8 +195,7 @@ class TestFrameData2CCDData:
         assert_equal(ccd.data, 100*np.ones(self.shape))
         assert_equal(ccd.unit, self.unit)
         for i in self.meta.keys():
-            ni = fits.Card.normalize_keyword(i)
-            assert_equal(ccd.meta[ni], self.meta[i])
+            assert_equal(ccd.meta[i], self.meta[i])
         assert_equal(ccd.mask, self.mask)
         assert_equal(ccd.uncertainty.array, np.ones(self.shape))
         assert_equal(ccd.uncertainty.unit, self.unit)