aff502d90084a93fc92fb0a749af430f18ed69b3
Julio Campagnolo <juliocampagnolo@gmail.com>
2024-05-11 17:09:51 -0300

    CI: fix typo

---
+++ tests/test_framedata_cachemanager.py
@@ -0,0 +1,6 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# flake8: noqa: F403, F405
+
+import os
+import pytest
+from astropop.framedata.cache_manager import TempDir, BaseTempDir
